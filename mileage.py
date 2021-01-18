3cca7036cb232012133228dbb87625a7eab59896
Jackie Whitworth <jackie.whitworth@gmail.com>
2021-01-18 10:28:40 -0800

    edited text

--- mileage.py
+++ mileage.py
@@ -1,4 +1,4 @@
 print("How many miles did you walk today?")
 miles = float(input())
 kms = miles * 1.60934
-print(f"Okay, you walked {miles} miles. That is equal to {round(kms, 2)} kilometers.")
+print(f"Got it, you walked {miles} miles. That is equal to {round(kms, 2)} kilometers.")
