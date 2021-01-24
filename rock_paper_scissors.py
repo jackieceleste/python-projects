e3d179a663ddd4eda194924ee6750348fd30295f
Jackie Whitworth <jackie.whitworth@gmail.com>
2021-01-24 10:47:48 -0800

    feat: add computer player in rock_paper_scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,26 +1,35 @@
+import random
+random_number = random.randint(1,3)
+if random_number == 1:
+	computer = "rock"
+if random_number == 2:
+	computer = "paper"
+if random_number == 3:
+	computer = "scissors"
 
-player1 = input("Player 1: rock, paper, or scissors?\n").lower()
-print("**NO CHEATING**\n" * 30)
-
-player2 = input("Player 2: rock, paper, or scissors?\n").lower()
-if player1 == player2:
-	print(f"You both picked {player1}. Play again?")
-elif player1 == "rock":
-	if player2 == "paper":
-		print (f"Player 1 picked {player1} and player 2 picked {player2}. Player 2 wins!")
-	elif player2 == "scissors":
-		print (f"Player 1 picked {player1} and player 2 picked {player2}. Player 1 wins!")
-elif player1 == "paper":
-	if player2 == "rock":
-		print(f"Player 1 picked {player1} and player 2 picked {player2}. Player 1 wins!")
-	elif player2 == "scissors":
-		print(f"Player 1 picked {player1} and player 2 picked {player2}. Player 2 wins!")
-elif player1 == "scissors":
-	if player2 == "rock":
-		print(f"Player 1 picked {player1} and player 2 picked {player2}. Player 2 wins!")
-	elif player2 == "paper":
-		print(f"Player 1 picked {player1} and player 2 picked {player2}. Player 1 wins!")
-else:
-	print("Something went wrong. Play again?")
+while True:
+	player = input("Rock, paper, or scissors?\n").lower()
+	if computer == player:
+		print(f"You both picked {player}. Play again?")
+	elif computer == "rock":
+		if player == "paper":
+			print (f"The computer picked {computer} and you picked {player}. You win!")
+		elif player == "scissors":
+			print (f"The computer picked {computer} and you picked {player}. The computer wins!")
+	elif computer == "paper":
+		if player == "rock":
+			print(f"The computer picked {computer} and you picked {player}. The computer wins!")
+		elif player == "scissors":
+			print(f"The computer picked {computer} and you picked {player}. You win!")
+	elif computer == "scissors":
+		if player == "rock":
+			print(f"The computer picked {computer} and you picked {player}. You win!")
+		elif player == "paper":
+			print(f"The computer picked {computer} and you picked {player}. The computer wins!")
+	else:
+		print("Something went wrong. Play again?")
+	again = input("Play again? (y/n) ")
+	if again == "n":
+		break
 
 
