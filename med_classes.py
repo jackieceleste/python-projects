51b1a5c3df54164fb96ed5a5f601d6cbcbde1fb8
Jackie Whitworth <jackie.whitworth@gmail.com>
2021-02-04 18:16:41 -0800

    add estimated insurance cost method

--- med_classes.py
+++ med_classes.py
@@ -1,12 +1,16 @@
 class Patient:
-  def __init__(self, name, age, sex, bmi, num_of_children, smoker):
-    self.name = name
-    self.age = age
-    # add more parameters here
-    self.sex = sex
-    self.bmi = bmi
-    self.num_of_children = num_of_children
-    self.smoker = smoker
+	def __init__(self, name, age, sex, bmi, num_of_children, smoker):
+	    self.name = name
+	    self.age = age
+	    # add more parameters here
+	    self.sex = sex
+	    self.bmi = bmi
+	    self.num_of_children = num_of_children
+	    self.smoker = smoker
+
+	def estimated_insurance_cost(self):
+		estimated_cost = 250 * self.age - 128 * self.sex + 370 * self.bmi + 425 * self.num_of_children + 24000 * self.smoker - 12500
+		print("{}'s estimated insurance cost is {} dollars.".format(self.name, estimated_cost))
 
 patient1 = Patient("John Doe", 25, 1, 22.2, 0, 0)
-print(patient1.name, patient1.age, patient1.sex, patient1.bmi, patient1.num_of_children, patient1.smoker)
+patient1.estimated_insurance_cost()
