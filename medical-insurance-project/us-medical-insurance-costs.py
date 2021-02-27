f410fe818eaf3f86f4a411ba1f502a2675122d1e
Jackie Whitworth <jackie.whitworth@gmail.com>
2021-02-26 20:43:14 -0800

    Add loop to magic 8 ball project

---
+++ medical-insurance-project/us-medical-insurance-costs.py
@@ -0,0 +1,51 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "# U.S. Medical Insurance Costs"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
+
+
+import csv
+
+with open("insurance.csv") as csv_file:
+  csv_reader = reader(csv_file)
+print(csv_reader)
+
+
+#Average age of patientes in dataset
+
+
+
+#Where most people are from
+
+
+#Cost for smokers vs. non-smokers
+
+#Average age if inidividual has 1 or more children
